39b2ff1fbc2fc35344042cd6cf2253b72cbadb8b
Oleg Kondrashov <condrashov.oleg@gmail.com>
2022-11-04 10:27:06 +0100

    finished Billing and CardDeal sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({styles}) => {
     return (
-        <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary putline-none ${styles}`}>
+        <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary putline-none ${styles} rounded-[10px]`}>
             Get Started
         </button>
     );

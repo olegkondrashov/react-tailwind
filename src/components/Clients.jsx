abcdc8fe435736a4295049a3d58dba88efa5c4b0
Oleg Kondrashov <condrashov.oleg@gmail.com>
2022-11-09 09:58:44 +0100

    finished Feedback and Clients sections

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import { clients } from "../constants";
+import styles from "../style";
 
-const Clients = () => {
-    return (
-        <div>
-            Clients
+const Clients = () => (
+    <section className={`${styles.flexCenter} my-4`}>
+        <div className={`${styles.flexCenter} flex-wrap w-full`}>
+            {clients.map((client) => (
+                <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] h-[40px] sm:h-[70px] mt-[10px] mb-[10px] hover:bg-gradient-to-r hover:from-white hover:to-cyan-300 rounded-full cursor-pointer`}>
+                    <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain"/>
+                </div>
+            ))}
         </div>
-    );
-};
+    </section>
+)
 
 export default Clients;
